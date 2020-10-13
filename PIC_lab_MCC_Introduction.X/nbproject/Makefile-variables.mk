42aef647bdaf1cba9b540c1c8d4f1810b34c2257
jvourv <jvourv@hotmail.com>
2020-10-13 15:20:00 +0300

    Initial commit

---
+++ PIC_lab_MCC_Introduction.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PIC_lab_MCC_Introduction.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC_lab_MCC_Introduction.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=piclabmccintroduction.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/piclabmccintroduction.x.tar
